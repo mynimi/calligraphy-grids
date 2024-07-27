aa56707c5793c7b42d7c6ee2f558e394ccc40606
Myriam <myriam@halfapx.com>
2024-07-28 00:26:09 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -3,30 +3,35 @@ export { CalligraphyLinePage } from './CalligraphyLineMaker';
 export { DotGridPage } from './DotGridMaker';
 export { GraphGridPage } from './GraphGridMaker';
 export { GridMaker } from './GridMaker';
+
 export type {
   GridPageBasicOptions,
   GridPageExtendedOptions,
   GridPageTechnicalOptions,
   GridPageConfig,
 } from './GridMaker';
+
 export type {
   CalligraphyAreaPageBasicOptions,
   CalligraphyAreaPageExtendedOptions,
   CalligraphyAreaPageTechOptions,
   CalligraphyAreaPageConfig,
 } from './CalligraphyAreaMaker';
+
 export type {
   CalligraphyLinePageBasicOptions,
   CalligraphyLinePageExtendedOptions,
   CalligraphyLinePageTechOptions,
   CalligraphyLinePageConfig,
 } from './CalligraphyLineMaker';
+
 export type {
   DotGridPageBasicOptions,
   DotGridPageExtendedOptions,
   DotGridPageTechOptions,
   DotGridPageConfig,
 } from './DotGridMaker';
+
 export type {
   GraphGridPageBasicOptions,
   GraphGridPageExtendedOptions,
