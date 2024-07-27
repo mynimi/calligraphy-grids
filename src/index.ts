78b0140ea9dbab67894eaa8a069fd626495d686e
Myriam <myriam@halfapx.com>
2024-07-27 21:27:20 +0200

    simplify setup

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-export { CalligraphyAreaPage } from './grids/CalligraphyAreaMaker';
-export { CalligraphyLinePage } from './grids/CalligraphyLineMaker';
-export { DotGridPage } from './grids/DotGridMaker';
-export { GraphGridPage } from './grids/GraphGridMaker';
+export { CalligraphyAreaPage } from './CalligraphyAreaMaker';
+export { CalligraphyLinePage } from './CalligraphyLineMaker';
+export { DotGridPage } from './DotGridMaker';
+export { GraphGridPage } from './GraphGridMaker';
+export type { CalligraphyAreaPageBasicOptions, CalligraphyAreaPageExtendedOptions, CalligraphyAreaPageTechOptions, CalligraphyAreaPageConfig } from './CalligraphyAreaMaker';
+export type { CalligraphyLinePageBasicOptions, CalligraphyLinePageExtendedOptions, CalligraphyLinePageTechOptions, CalligraphyLinePageConfig } from './CalligraphyLineMaker';
+export type { DotGridPageBasicOptions, DotGridPageExtendedOptions, DotGridPageTechOptions, DotGridPageConfig } from './DotGridMaker';
+export type { GraphGridPageBasicOptions, GraphGridPageExtendedOptions, GraphGridPageTechOptions, GraphGridPageConfig } from './GraphGridMaker';
