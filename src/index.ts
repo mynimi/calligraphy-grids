44984789307822c7ec4c1f502486bb77b4f0216e
Myriam <myriam@halfapx.com>
2024-07-27 23:49:00 +0200

    fix(exports): add missing exports

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,34 @@ export { CalligraphyAreaPage } from './CalligraphyAreaMaker';
 export { CalligraphyLinePage } from './CalligraphyLineMaker';
 export { DotGridPage } from './DotGridMaker';
 export { GraphGridPage } from './GraphGridMaker';
-export type { CalligraphyAreaPageBasicOptions, CalligraphyAreaPageExtendedOptions, CalligraphyAreaPageTechOptions, CalligraphyAreaPageConfig } from './CalligraphyAreaMaker';
-export type { CalligraphyLinePageBasicOptions, CalligraphyLinePageExtendedOptions, CalligraphyLinePageTechOptions, CalligraphyLinePageConfig } from './CalligraphyLineMaker';
-export type { DotGridPageBasicOptions, DotGridPageExtendedOptions, DotGridPageTechOptions, DotGridPageConfig } from './DotGridMaker';
-export type { GraphGridPageBasicOptions, GraphGridPageExtendedOptions, GraphGridPageTechOptions, GraphGridPageConfig } from './GraphGridMaker';
+export { GridMaker } from './GridMaker';
+export type {
+  GridPageBasicOptions,
+  GridPageExtendedOptions,
+  GridPageTechnicalOptions,
+  GridPageConfig,
+} from './GridMaker';
+export type {
+  CalligraphyAreaPageBasicOptions,
+  CalligraphyAreaPageExtendedOptions,
+  CalligraphyAreaPageTechOptions,
+  CalligraphyAreaPageConfig,
+} from './CalligraphyAreaMaker';
+export type {
+  CalligraphyLinePageBasicOptions,
+  CalligraphyLinePageExtendedOptions,
+  CalligraphyLinePageTechOptions,
+  CalligraphyLinePageConfig,
+} from './CalligraphyLineMaker';
+export type {
+  DotGridPageBasicOptions,
+  DotGridPageExtendedOptions,
+  DotGridPageTechOptions,
+  DotGridPageConfig,
+} from './DotGridMaker';
+export type {
+  GraphGridPageBasicOptions,
+  GraphGridPageExtendedOptions,
+  GraphGridPageTechOptions,
+  GraphGridPageConfig,
+} from './GraphGridMaker';
